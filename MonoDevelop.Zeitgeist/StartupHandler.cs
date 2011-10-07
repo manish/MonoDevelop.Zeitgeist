660ed418b46663883e29eed81e5929652a4c069b
Patrick McEvoy <patrick@qmtech.net>
2011-10-07 14:05:54 +0100

    Add MIT license

--- MonoDevelop.Zeitgeist/StartupHandler.cs
+++ MonoDevelop.Zeitgeist/StartupHandler.cs
@@ -1,3 +1,29 @@
+//
+//  StartupHandler.cs
+//
+//  Author:
+//       Patrick McEvoy <patrick@qmtech.net>
+//
+//  Copyright (c) 2011 QMTech.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify,
+// merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 using System;
 using Mono.Addins;
 using MonoDevelop.Components.Commands;
@@ -34,7 +60,7 @@ namespace MonoDevelop.Zeitgeist
 			if (!e.Document.IsFile)
 				return;
 
-			MonoDevelop.Core.LoggingService.LogInfo ("== MonoDevelop.Zeitgeist : Opened {0}", e.Document.FileName.FileName);
+			MonoDevelop.Core.LoggingService.LogInfo ("Logging access of {0} to Zeitgeist", e.Document.FileName.FileName);
 			var tracked = new TrackedDocument (client, e.Document);
 			tracked.Destroyed += delegate {
 				foreach (var t in documents)
